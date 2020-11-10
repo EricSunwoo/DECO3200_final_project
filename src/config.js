1738c5e4d96b13b0cc4e93dd5fc8e3629f9a70d9
Eric <ericsunwoodoh@gmail.com>
2020-11-10 12:12:33 +1100

    test1

--- src/config.js
+++ src/config.js
@@ -2,3 +2,5 @@ import { projects } from 'json!../.firebaserc'
 
 export const apiKey = "AIzaSyBO4S29MhYLOPQCHKxUjVBpIrYGiH6NMlI"
 export const projectId = projects.default
+
+export const siteName = "Greenhouse"
